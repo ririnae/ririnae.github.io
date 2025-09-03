c1ccaab80b951712d935b820cfa4ab791883c0b4
ririnae <princessinoc2@gmail.com>
2025-09-04 00:06:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I love you with everything I am.\n\nAs if my heart knows no limits, yet every beat cuts deeper, a sweetness threaded can ace. \n\nWe both tried. We both cared. But the devotion is heavy— it crushes as much as it carries, but I can’t set it down.\n\nIt clings, it consumes, a storm I never escape.\n\nThe world fades to shadow, faces blur, voices vanish, and still, unrelenting, my gaze returns only to you.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,3 +21,4 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
