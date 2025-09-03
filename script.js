a2ef54125a8df2c838c8d95cf49273f8d7889bbd
ririnae <princessinoc2@gmail.com>
2025-09-04 00:12:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I love you with everything I am.\n\nAs if my heart knows no limits, yet every beat cuts deeper, a sweetness threaded can ace. \n\nWe both tried. We both cared. But the devotion is heavy— it crushes as much as it carries, but I can’t set it down.\n\nIt clings, it consumes, a storm I never escape.\n\nThe world fades to shadow, faces blur, voices vanish, and still, unrelenting, my gaze returns only to you.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I love you with everything I am.\n\nAs if my heart knows no limits, yet every beat cuts deeper, a sweetness threaded can ache. \n\nThe world fades to shadow, faces blur, voices vanish, and still, unrelenting, my gaze returns only to you.`;
 
 function showLetter() {
-  document.getElementById("introText").style.opacity = 0;
+  document.getElementById("introText").style.opacity = 0
   document.querySelector(".btn").style.display = "none";
 
   setTimeout(() => {
@@ -21,4 +21,5 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
